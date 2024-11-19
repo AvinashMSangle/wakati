b8e8827c5156f5211e244025446da40ebdcfaaff
AvinashMSangle <avinashMS.3119@proton.me>
2024-11-19 07:56:38 +0000

    updated

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,38 @@
-import { Hono } from 'hono'
+import { Hono } from 'hono';
+import { logger } from 'hono/logger';
+import { cors } from 'hono/cors';
 
-const app = new Hono()
+const app = new Hono();
+
+app.use(logger());
+app.use(cors());
+const wpm=238;
+
+app.get('/status', (c) => {
+  return c.json({message:"API is Active"});
+});
 
 app.get('/', (c) => {
-  const text = c.req.query('text')
-  return c.json({ text: text?.split(/\s+/).length})
-})
+  const text = c.req.query("text");
 
-export default app
+  if (!text) {
+    return c.json({message:"Please provide a text"});
+  }
+  
+  const wordCount = text.split(/\s+/).length;
+
+  return c.json({wordCount});
+
+});
+
+app.post('/',async (c) => {
+  const {text} = await c.req.json();
+
+  if (!text) {
+    return c.json({message:"Please provide a text"}, 400);
+  }
+
+  return c.json({count: text.split(/\s+/).length},400);
+});
+
+export default app;
